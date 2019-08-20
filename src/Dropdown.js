f4501d9ef4fd0b95a40edd6db0d15502ac532a20
Annah Isenberg <annahisenberg@gmail.com>
2019-08-19 19:32:08 -0500

    blah

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,62 +2,69 @@ import React, { Component } from 'react'
 
 export default class Dropdown extends Component {
     constructor() {
-      super();
-  
-      this.handleClick = this.handleClick.bind(this);
-      this.handleOutsideClick = this.handleOutsideClick.bind(this);
-      this.closeLightbox = this.closeLightbox.bind(this);
-  
-      this.state = {
-        popupVisible: false
-      };
-    }
-  
-    handleClick() {
-      if (!this.state.popupVisible) {
-        // attach/remove event handler
-        document.addEventListener('click', this.handleOutsideClick, false);
-      } else {
-        document.removeEventListener('click', this.handleOutsideClick, false);
-      }
-  
-      this.setState(prevState => ({
-         popupVisible: !prevState.popupVisible,
-      }));
-    }
-    
-    handleOutsideClick(e) {
-      // ignore clicks on the component itself
-      if (this.node.contains(e.target)) {
-        return;
-      }
-      
-      this.handleClick();
+        super();
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+        this.closeLightboxOpenModal = this.closeLightboxOpenModal.bind(this);
+
+        this.state = {
+            popupVisible: false,
+            openModal: false
+        };
     }
 
-    closeLightbox() {
+    handleClick() {
+        if (!this.state.popupVisible) {
+            // attach/remove event handler
+            document.addEventListener('click', this.handleOutsideClick, false);
+        } else {
+            document.removeEventListener('click', this.handleOutsideClick, false);
+        }
+
+        this.setState(prevState => ({
+            popupVisible: !prevState.popupVisible,
+        }));
+    }
+
+    handleOutsideClick(e) {
+        // ignore clicks on the component itself
+        if (this.node.contains(e.target)) {
+            return;
+        }
+
+        this.handleClick();
+    }
+
+    closeLightboxOpenModal() {
         this.setState({
-            popupVisible: false
+            popupVisible: false,
+            openModal: !this.state.openModal
         })
     }
-  
+
     render() {
-      return (
-        <div className="popover-container" ref={node => { this.node = node; }}>
-          <button
-            onClick={this.handleClick}
-          >
-            Toggle Popover
-          </button>
-          {this.state.popupVisible && (
-            <div
-              className="popover"
-              style={{background: "purple", padding: "5rem"}}
-            >
-              <p onClick={this.closeLightbox} style={{background: "lightgray"}}>I'm a popover</p>
+        let firstLightbox;
+        if (this.state.popupVisible) {
+            firstLightbox = (
+                <div className="popover" style={{ background: "purple", padding: "5rem" }}>
+                    <p onClick={this.closeLightboxOpenModal} style={{ background: "lightgray" }}>I'm a popover</p>
+                </div>
+            )
+        }else{
+            firstLightbox =null;
+        }
+
+        return (
+            <div className="popover-container" ref={node => { this.node = node; }}>
+                <button onClick={this.handleClick}>Toggle Popover</button>
+                {firstLightbox}
+                {
+                    this.state.openModal && (
+                        <div style={{ border: "1px solid black", padding: "4rem", background: "lightpink" }}>I'm a modal</div>
+                    )
+                }
             </div>
-           )}
-        </div>
-      );
+        );
     }
-  }
+}
